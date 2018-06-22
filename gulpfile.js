32d89fe0e68746e29905be78c1b4805c514dc2bd
yongqiang chen <1059173813@qq.com>
2018-06-22 15:22:01 +0800

    第二次提交

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,17 @@
 const gulp = require("gulp"),
-	connect = require("gulp-connect");
+	connect = require("gulp-connect"),
+	sass = require("gulp-sass");
 
 gulp.task("connect", function(){
 	connect.server({
 		root : "src"
 	});
+});
+
+//定制任务：编译sass
+gulp.task("sass-task", function(){
+	gulp.src("src/sass/*.scss")//源
+		.pipe(sass({outputStyle:"expanded"}))
+		.pipe(gulp.dest("dist/css/"))
+		.pipe(connect.reload());
 });
